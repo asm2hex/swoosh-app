3d68bf7ce2979a03e482442198e3f57ba3ecead4
Lancelot Edwards <lancelot.edwards@gmail.com>
2017-08-07 14:52:04 -0500

    add assets for and design welcome screen

---
+++ swoosh-app/View/BorderButton.swift
@@ -0,0 +1,20 @@
+//
+//  BorderButton.swift
+//  swoosh-app
+//
+//  Created by Lancelot Edwards on 07/08/2017.
+//  Copyright © 2017 Lancelot Edwards. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    
+    layer.borderWidth = 3.0
+    layer.borderColor = UIColor.white.cgColor
+  }
+
+}
