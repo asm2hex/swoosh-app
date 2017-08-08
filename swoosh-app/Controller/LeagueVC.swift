094c38a295a052cfd438ffa2f949262a04c59fa7
Lancelot Edwards <lancelot.edwards@gmail.com>
2017-08-08 09:57:46 -0500

    add model file and add logic for buttons in league VC

--- swoosh-app/Controller/LeagueVC.swift
+++ swoosh-app/Controller/LeagueVC.swift
@@ -10,13 +10,40 @@ import UIKit
 
 class LeagueVC: UIViewController {
   
+  @IBOutlet weak var nextButton: BorderButton!
+  
+  var player: Player!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    player = Player()
+  }
+  
+  @IBAction func selectedLeague(_ sender: UIButton) {
+    guard let buttonTitle = sender.title(for: .selected) else { return }
     
-    // Do any additional setup after loading the view.
-  }  
+    switch buttonTitle {
+    case "Mens":
+      selectLeague(leagueType: "mens")
+    case "Womens":
+      selectLeague(leagueType: "womens")
+    case "Co-ed":
+      selectLeague(leagueType: "coed")
+    default:
+      break
+    }
+    
+  }
+  
   
   @IBAction func onNextTapped(_ sender: Any) {
     performSegue(withIdentifier: "skillVCSegue", sender: self)
   }
+
+  func selectLeague(leagueType: String) {
+    player.desiredLeague = leagueType
+    nextButton.isEnabled = true
+  }
+
+
 }
---
+++ swoosh-app/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  swoosh-app
+//
+//  Created by Lancelot Edwards on 08/08/2017.
+//  Copyright © 2017 Lancelot Edwards. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+  var desiredLeague: String?
+  var selectedSkillLevel: String?
+}
